ae47407707b16e9270c21e80e5df288953736a4f
Brandon Shar <brandon.shar@icloud.com>
2020-04-28 15:14:41 -0400

    only selectively json encode query string params

--- example/src/App.js
+++ example/src/App.js
@@ -12,6 +12,14 @@ const App = () => {
     updateQueryString({text: textValue})
   }, 2000, [textValue])
 
+  const addArray = () => updateQueryString({
+    arr: [1, 2, 3]
+  })
+
+  const addObject = () => updateQueryString({
+    obj: {color: 'green'}
+  })
+
   return (
     <div>
       <h2>Debounce Test</h2>
@@ -20,8 +28,10 @@ const App = () => {
       <p>{exampleValue}</p>
       <h2>Toggle Test</h2>
       <button style={{width: '100px', height: '100px', backgroundColor: onOff ? 'green' : 'red'}} onClick={toggle}>{onOff ? 'On' : 'Off'}</button>
-    
+
       <h2>Query String Test</h2>
+      <button onClick={addArray}>Add an array</button>
+      <button onClick={addObject}>Add an object</button>
       <p>{JSON.stringify(queryString)}</p>
     </div>
   )
--- src/useQueryString.js
+++ src/useQueryString.js
@@ -1,16 +1,25 @@
 import {useLocation, useHistory} from 'react-router-dom'
 
+const encodeValue = value => (value && typeof value === 'object') ? JSON.stringify(value) : value
+const parseValue = value => {
+  try {
+    return JSON.parse(value)
+  } catch {
+    return value
+  }
+}
+
 function useQueryString() {
   const currentQueryString = new URLSearchParams(useLocation().search)
   const queryParams = [...currentQueryString.entries()].reduce(
-    (qs, [key, value]) => ({...qs, [key]: JSON.parse(value)}),
+    (qs, [key, value]) => ({...qs, [key]: parseValue(value)}),
     {}
   )
   const history = useHistory()
 
   const setQueryString = (params) => {
     const processedParams = Object.keys(params).reduce((processing, key) => {
-      return {...processing, [key]: JSON.stringify(params[key])}
+      return {...processing, [key]: encodeValue(params[key])}
     }, {})
     const newParams = new URLSearchParams({...currentQueryString, ...processedParams})
     history.replace(`?${newParams}`)
